e9d2c9c609560cdb4eb538d912d0b30afa8b48f3
tema-glumskov <okoyom@inbox.ru>
2024-05-05 14:29:12 +0300

    Ура победа

--- file.py
+++ file.py
@@ -215,24 +215,24 @@ def train_step(model: torch.nn.Module,
         # 1. Шаг вперед
         y_pred = model(X)
 
-        # 2. Calculate  and accumulate loss
+        # 2. Посчитать и собрать ошибки при обучении
         loss = loss_fn(y_pred, y)
         train_loss += loss.item() 
 
-        # 3. Optimizer zero grad
+        # 3. Оптимизатор делает шаг
         optimizer.zero_grad()
 
-        # 4. Loss backward
+        # 4. Происходит обратный проход
         loss.backward()
 
-        # 5. Optimizer step
+        # 5. Шаг оптимизатора
         optimizer.step()
 
-        # Calculate and accumulate accuracy metric across all batches
+        # Вычислить точность и добавить в сумму точностей
         y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
         train_acc += (y_pred_class == y).sum().item()/len(y_pred)
 
-    # Adjust metrics to get average loss and accuracy per batch 
+    # Получение среднего значения ошибки и точности
     train_loss = train_loss / len(dataloader)
     train_acc = train_acc / len(dataloader)
     return train_loss, train_acc
